e36df2846dda171d11009a725320ff68bb153c24
Francisco Garcia <jjjchico1@gmail.com>
2023-04-11 10:19:04 -0600

    feat: animation MyInfo component

--- src/pages/home/components/MyInfo/index.jsx
+++ src/pages/home/components/MyInfo/index.jsx
@@ -1,20 +1,43 @@
+import { motion } from 'framer-motion'
 import { SocialMedia } from '../SocialMedia'
 import './index.scss'
 
+const variants = {
+  hidden: { opacity: 0, y: -50 },
+  visible: { opacity: 1, y: 0 }
+}
+
 export const MyInfo = () => {
   return (
-    <div className="my-info">
+    <motion.div
+      className="my-info"
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 0.7, delay: 0.3 }}
+    >
       <div className="my-info__header">
         <SocialMedia />
-        <span>SOBRE MI</span>
+        <motion.span
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
+          SOBRE MI
+        </motion.span>
       </div>
-      <p>
+      <motion.p
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7, delay: 1 }}
+      >
         Saludos, mi nombre es Francisco Garcia, desarrollador web full stack con
         amplia experiencia en el diseño y desarrollo de sitios web
         personalizados desde cero. Mi pasión por la tecnología me ha llevado a
         mantenerme actualizado y en constante aprendizaje para mejorar mis
         habilidades y ofrecer soluciones de alta calidad.
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   )
 }
--- src/pages/home/components/SocialMedia/index.jsx
+++ src/pages/home/components/SocialMedia/index.jsx
@@ -1,27 +1,48 @@
+import { motion } from 'framer-motion'
 import './index.scss'
 
 const ITEMS = [
   {
     label: 'LinkedIn',
-    url: 'https://www.linkedin.com/in/francisco-garcia-94a2a2232/'
+    url: 'https://www.linkedin.com/in/francisco-garcia-94a2a2232/',
+    delay: 1.1
   },
   {
     label: 'GitHub',
-    url: 'https://github.com/francisco-JGC'
+    url: 'https://github.com/francisco-JGC',
+    delay: 1.4
   }
 ]
+
+const variants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0 }
+}
 
 export const SocialMedia = () => {
   return (
     <div className="social-media">
-      <span>SOCIAL</span>
+      <motion.span
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5, delay: 0.7 }}
+      >
+        SOCIAL
+      </motion.span>
 
       <ul className="social-media__list">
         {ITEMS.map((item, index) => (
-          <li key={index}>
+          <motion.li
+            key={index}
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 0.5, delay: item.delay }}
+          >
             {item.label}
             {index !== ITEMS.length - 1 && <span>|</span>}
-          </li>
+          </motion.li>
         ))}
       </ul>
     </div>
