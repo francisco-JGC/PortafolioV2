25594c8f3d7449dbf776f6c596a5bcf2af256ba5
Francisco Garcia <jjjchico1@gmail.com>
2023-04-14 02:19:21 -0600

    fix: redirect logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ import './index.scss'
 export const Header = () => {
   return (
     <header className="header-container">
-      <Link className="logo-container">
+      <Link className="logo-container" to="/">
         <img src={logo} alt="Chico Dev Logo" />
         <span>CHICO DEV</span>
       </Link>
