5b0f557ea450f11bf2282f2fdff5d75ff971afb8
Francisco Garcia <jjjchico1@gmail.com>
2023-04-22 09:18:13 -0600

    feat: include AOS and contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.scss'
+
+import AOS from 'aos'
+import 'aos/dist/aos.css' // You can also use <link> for styles
+
 import { Header } from './components/Header'
 import { Layout } from './components/Layout'
 import { AboutPage } from './pages/about'
+import { ContactPage } from './pages/contact'
 import { HomePage } from './pages/home'
 
 const App = () => {
+  AOS.init()
+
   return (
     <div className="App">
       <Header />
@@ -12,6 +19,7 @@ const App = () => {
       <Layout>
         <HomePage />
         <AboutPage />
+        <ContactPage />
       </Layout>
 
       {/* <DescNavigation /> */}
