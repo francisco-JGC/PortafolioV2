b40cabddc7e658d56cecfd698e1454a98a4371be
Francisco Garcia <jjjchico1@gmail.com>
2023-10-13 14:41:56 -0600

    feat: services page component

---
+++ src/pages/services/container/index.jsx
@@ -0,0 +1,14 @@
+import './index.scss'
+
+export const ServicePages = () => {
+  return (
+    <div className="service-pages" id="services">
+      <span>
+        <small>Servicios {' />'}</small>
+        Seleccione un servicio, Diseño UI, APIs, System web...
+      </span>
+
+      <div className="service-pages__container"></div>
+    </div>
+  )
+}
