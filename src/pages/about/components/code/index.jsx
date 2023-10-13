06428f95b80598852742a2618b3ba49083bb030e
Francisco Garcia <jjjchico1@gmail.com>
2023-10-13 14:38:14 -0600

    feat: add more information

--- src/pages/about/components/code/index.jsx
+++ src/pages/about/components/code/index.jsx
@@ -3,114 +3,102 @@ import './index.scss'
 export const Code = () => {
   return (
     <div className="code-container">
-      <div className="code-line jump" data-line-nr="1" data-aos="zoom-in-up">
+      <div className="code-line jump" data-line-nr="1">
         class Francisco Garcia &#123;
       </div>
-      <div className="code-line jump" data-line-nr="2" data-aos="zoom-in-down">
+      <div className="code-line jump" data-line-nr="2">
         <div className="dot">. .</div>
       </div>
-      <div className="code-line" data-line-nr="3" data-aos="zoom-in-down">
+      <div className="code-line" data-line-nr="3">
         <div className="dot">. .</div>
         constructor<span className="code-key">() &#123;</span>
       </div>
-      <div
-        className="code-line"
-        data-line-nr="4"
-        data-decoration-line="|"
-        data-aos="zoom-in-up"
-      >
+      <div className="code-line" data-line-nr="4" data-decoration-line="|">
         <div className="dot">. . . .</div>
         this.<span className="code-indent">nombre</span>={' '}
         <span className="code-text">'Francisco Garcia'</span>
       </div>
-      <div
-        className="code-line"
-        data-line-nr="5"
-        data-decoration-line="|"
-        data-aos="zoom-in-down"
-      >
+      <div className="code-line" data-line-nr="5" data-decoration-line="|">
         <div className="dot">. . . .</div>
         this.<span className="code-indent">correo</span> ={' '}
         <span className="code-text">'jjjchico1@gmail.com'</span>
       </div>
-      <div
-        className="code-line"
-        data-line-nr="6"
-        data-decoration-line="|"
-        data-aos="zoom-in-up"
-      >
+      <div className="code-line" data-line-nr="6" data-decoration-line="|">
         <div className="dot"> . . . .</div>
         this.<span className="code-indent">localidad</span> ={' '}
         <span className="code-text">'León, Nicaragua'</span>
       </div>
-      <div className="code-line jump" data-line-nr="7" data-aos="zoom-in-down">
+      <div className="code-line jump" data-line-nr="7">
         <div className="dot">. .</div>
         <span className="code-key">&#125;</span>
       </div>
-      <div className="code-line jump" data-line-nr="8" data-aos="zoom-in-up">
+      <div className="code-line jump" data-line-nr="8">
         <div className="dot">. .</div>
       </div>
-      <div className="code-line" data-line-nr="9" data-aos="zoom-in-down">
+      <div className="code-line" data-line-nr="9">
         <div className="dot">. .</div>
         Experiencia profesional <span className="code-key">() &#123;</span>
       </div>
-      <div
-        className="code-line"
-        data-line-nr="10"
-        data-decoration-line="|"
-        data-aos="zoom-in-up"
-      >
+      <div className="code-line" data-line-nr="10" data-decoration-line="|">
         <div className="dot">. . . .</div>
         return <span className="code-indent">[</span>
       </div>
-      <div className="code-line" data-line-nr="11" data-aos="zoom-in-down">
+      <div className="code-line" data-line-nr="11">
         <div className="dot">. . . . . . .</div>
         &#123;
         <span className="code-line code-text data-key" data-decoration-line="|">
-          '2023-Actual'
-        </span>
-        : <span className="code-line code-text">'Diseñador Web'</span> &#125;,
-      </div>
-      <div className="code-line" data-line-nr="12" data-aos="zoom-in-up">
-        <div className="dot">. . . . . . .</div>
-        &#123;
-        <span className="code-line code-text data-key" data-decoration-line="|">
-          '2020-2022'
+          '04.2023 - actual'
         </span>
         :{' '}
-        <span className="code-line code-text">'Desarrollador Full-Stack'</span>{' '}
+        <span className="code-line code-text">
+          'Full-Stack Developer en REGXI.SA'
+        </span>{' '}
+        &#125;,
+      </div>
+      <div className="code-line" data-line-nr="12">
+        <div className="dot">. . . . . . .</div>
+        &#123;
+        <span className="code-line code-text data-key" data-decoration-line="|">
+          '01.2023 - 04.2023'
+        </span>
+        :{' '}
+        <span className="code-line code-text">
+          'Front End Developer como Freelance'
+        </span>{' '}
+        &#125;,
+      </div>
+      <div className="code-line" data-line-nr="13">
+        <div className="dot">. . . . . . .</div>
+        &#123;
+        <span className="code-line code-text data-key" data-decoration-line="|">
+          '12.2020 - 12.2022'
+        </span>
+        :{' '}
+        <span className="code-line code-text">
+          'Full-Stack Developer en TI House'
+        </span>{' '}
         &#125;
       </div>
-      <div
-        className="code-line"
-        data-line-nr="13"
-        data-decoration-line="|"
-        data-aos="zoom-in-down"
-      >
+      <div className="code-line" data-line-nr="14" data-decoration-line="|">
         <div className="dot">. . . .</div>
         <span className="code-line code-indent">]</span>
       </div>
-      <div className="code-line" data-line-nr="14" data-aos="zoom-in-up">
+      <div className="code-line" data-line-nr="15">
         <div className="dot">. . </div>
         <span className="code-key">&#125;</span>
       </div>
-      <div className="code-line jump" data-line-nr="15" data-aos="zoom-in-down">
+      <div className="code-line jump" data-line-nr="16">
         <div className="dot">. .</div>
       </div>
-      <div className="code-line" data-line-nr="16" data-aos="zoom-in-up">
+      <div className="code-line" data-line-nr="17">
         <div className="dot">. .</div>
         Habilidades <span className="code-key">() &#123;</span>
       </div>
-      <div
-        className="code-line"
-        data-line-nr="17"
-        data-decoration-line="|"
-        data-aos="zoom-in-down"
-      >
+      <div className="code-line" data-line-nr="18" data-decoration-line="|">
         <div className="dot">. . . .</div>
         return <span className="code-indent">[</span>
       </div>
-      <div className="code-line" data-line-nr="18" data-aos="zoom-in-up">
+      <div className="code-line" data-line-nr="19">
         <div className="dot">. . . . . .</div>
         <div className="flex">
           <span className="code-line code-text" data-decoration-line="|">
@@ -121,60 +109,88 @@ export const Code = () => {
           <span className="code-line code-text">'UI/UX'</span>,
           <span className="code-line code-text">'Diseños animados'</span>,
           <span className="code-line code-text">'Node.js'</span>,
+          <span className="code-line code-text">'NestJS'</span>,
         </div>
       </div>
-      <div className="code-line" data-line-nr="19" data-aos="zoom-in-down">
+      <div className="code-line" data-line-nr="20">
         <div className="dot">. . . . . .</div>
         <div className="flex">
           <span className="code-line code-text" data-decoration-line="|">
             'ReactJS'
           </span>
           ,<span className="code-line code-text">'Typescript'</span>,
-          <span className="code-line code-text">'npm/yarn'</span>,
+          <span className="code-line code-text">'npm/yarn/pnpm'</span>,
+          <span className="code-line code-text">'NextJS'</span>,
           <span className="code-line code-text">'Git'</span>,
           <span className="code-line code-text">'Github'</span>,
           <span className="code-line code-text">'Ilustrador'</span>,
-          <span className="code-line code-text">'Adobe XD'</span>,
+          <span className="code-line code-text">'Figma'</span>,
         </div>
       </div>
-      <div className="code-line" data-line-nr="20" data-aos="zoom-in-up">
+      <div className="code-line" data-line-nr="21">
         <div className="dot">. . . . . .</div>
         <div className="flex">
           <span className="code-line code-text" data-decoration-line="|">
             'Photoshop'
           </span>
-          ,<span className="code-line code-text">'Bootstrap'</span>,
           <span className="code-line code-text">'Diseño responsivo'</span>,
           <span className="code-line code-text">'NestJS'</span>,
           <span className="code-line code-text">'MySQL'</span>,
+          <span className="code-line code-text">'PostgreSQL'</span>,
         </div>
       </div>
-      <div
-        className="code-line"
-        data-line-nr="21"
-        data-decoration-line="|"
-        data-aos="zoom-in-down"
-      >
+      <div className="code-line" data-line-nr="22" data-decoration-line="|">
         <div className="dot">. . . .</div>
         <span className="code-line code-indent">]</span>
       </div>
-      <div className="code-line" data-line-nr="22" data-aos="zoom-in-up">
+      <div className="code-line" data-line-nr="23">
         <div className="dot">. .</div>
         <span className="code-key">&#125;</span>
       </div>
 
-      <div className="code-line" data-line-nr="23" data-aos="zoom-in-down">
+      <div className="code-line" data-line-nr="24">
         <div className="dot"></div>
         &#125;
       </div>
 
-      {/* Experiencia profesional */}
+      {/* apartado de Educacion */}
+      <div className="code-line jump" data-line-nr="25">
+        <div className="dot">. .</div>
+      </div>
+      <div className="code-line" data-line-nr="26">
+        <div className="dot">. .</div>
+        Educación <span className="code-key">() &#123;</span>
+      </div>
+      <div className="code-line" data-line-nr="27" data-decoration-line="|">
+        <div className="dot">. . . .</div>
+        return <span className="code-indent">[</span>
+      </div>
+      <div className="code-line" data-line-nr="28">
+        <div className="dot">. . . . . . .</div>
+        &#123;
+        <span className="code-line code-text data-key" data-decoration-line="|">
+          '2018-2022'
+        </span>
+        :{' '}
+        <span className="code-line code-text">
+          'Ingeniería en Sistemas de Información'
+        </span>
+        &#125;
+      </div>
+      <div className="code-line" data-line-nr="30" data-decoration-line="|">
+        <div className="dot">. . . .</div>
+        <span className="code-line code-indent">]</span>
+      </div>
+      <div className="code-line" data-line-nr="31">
+        <div className="dot">. .</div>
+        <span className="code-key">&#125;</span>
+      </div>
 
-      <div className="code-line jump" data-line-nr="24" data-aos="zoom-in-up">
+      <div className="code-line jump" data-line-nr="32">
         <div className="dot">. .</div>
       </div>
 
-      <div className="code-line" data-line-nr="25" data-aos="zoom-in-down">
+      <div className="code-line" data-line-nr="33">
         <div className="dot"></div>
         export default Francisco Garcia
       </div>
