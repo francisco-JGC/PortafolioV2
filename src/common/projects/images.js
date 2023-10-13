d583c9275274861649458b84480a98d770a0c6b0
Francisco Garcia <jjjchico1@gmail.com>
2023-10-13 14:35:51 -0600

    feat: import iamges

--- src/common/projects/images.js
+++ src/common/projects/images.js
@@ -3,5 +3,15 @@ import moniben_store from '../../assets/images/MONIBEN-STORE.png'
 import napet_app from '../../assets/images/Napet-app.png'
 import chatapp from '../../assets/images/chatapp.jpg'
 import napet from '../../assets/images/napet.png'
+import exploraventura from '../../assets/images/exploraventuratravel.png'
+import exploraventura_light from '../../assets/images/exploraventuratravel-light.png'
 
-export { moniben_light, chatapp, napet, moniben_store, napet_app }
+export {
+  moniben_light,
+  chatapp,
+  napet,
+  moniben_store,
+  napet_app,
+  exploraventura,
+  exploraventura_light
+}
